be864d99137216eacb802728219ae5d7fc72dcb1
croxxy0 <marmagan001@gmail.com>
2026-01-04 09:36:10 +0000

    guncelleme yapildi

--- Nallıhan GO/menu-verileri.js
+++
@@ -1,9 +0,0 @@
-const dukkanlar = {
-    "meshur-nallihan-donercisi": [
-        { id: 1, ad: "Et Döner Dürüm", aciklama: "Özel soslu lavaş", fiyat: 120, img: "doner.jpg" },
-        { id: 2, ad: "Öğrenci Menüsü", aciklama: "Döner + Ayran", fiyat: 85, img: "menu.jpg" }
-    ],
-    "kampus-pizza": [
-        { id: 101, ad: "Karışık Pizza", aciklama: "Bol malzeme", fiyat: 150, img: "pizza.jpg" }
-    ]
-};
